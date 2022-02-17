4e3b258368282ace4dd266cac6dbc896719860bb
Lcastro98 <lcastro0398@gmail.com>
2022-02-17 18:19:56 -0500

    Se agregan los atributos, el constructor y los getters de la clase Song

--- src/musiclibrary/Song.java
+++ src/musiclibrary/Song.java
@@ -9,5 +9,59 @@ package musiclibrary;
  * @author Lorena Castro Lcastro0398@gmail.com
  */
 public class Song {
+    private int id;
+    private String name;
+    private String year;
+    private float duration;
+    private String genre;
+    private String cover;
+    private String description;
+    
+    public Song(
+        int id, 
+        String name, 
+        String year, 
+        float duration, 
+        String genre,
+        String cover, 
+        String description)
+    {
+        this.id = id;
+        this.name = name;
+        this.year = year;
+        this.duration = duration;
+        this.genre = genre;
+        this.cover = cover;
+        this.description = description;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public float getDuration() {
+        return duration;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public String getCover() {
+        return cover;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+    
     
 }
