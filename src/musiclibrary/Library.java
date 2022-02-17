578e120546d55ce8ee4a463c0ec7d4c8353ddcd8
Lcastro98 <lcastro0398@gmail.com>
2022-02-17 18:24:47 -0500

    Se agregan los atributos, el constructor y los getters de la clase Library

--- src/musiclibrary/Library.java
+++ src/musiclibrary/Library.java
@@ -4,10 +4,39 @@
  */
 package musiclibrary;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Lorena Castro Lcastro0398@gmail.com
  */
 public class Library {
     
+    public ArrayList<Song> songs;
+    
+    public Library(ArrayList<Song> songs) {
+        this.songs = songs;
+    }
+    
+    public ArrayList<Song> getSongs() {
+        return songs;
+    }
+
+    public void setSongs(ArrayList<Song> songs) {
+        this.songs = songs;
+    }
+    
+    public void addSong(Song s) {
+        this.songs.add(s);
+    }
+    
+    public Song getSong(int id) {
+        int index = 0;
+        for (int i = 0; i < songs.size(); i++) {
+            if (songs.get(i).getId()==id) {
+                index = i;
+            }
+        } 
+        return this.songs.get(index);
+    }
 }
