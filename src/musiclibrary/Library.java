759966b0cfac12c32a9812e5947c6de656703717
Lcastro98 <lcastro0398@gmail.com>
2022-02-17 18:08:16 -0500

    Se crea el proyecto y las clases

---
+++ src/musiclibrary/Library.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package musiclibrary;
+
+/**
+ *
+ * @author Lorena Castro Lcastro0398@gmail.com
+ */
+public class Library {
+    
+}
