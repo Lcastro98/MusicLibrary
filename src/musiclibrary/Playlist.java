332510ea2b32cd994bae4e67b62fc37da4cb00d8
wianpedi <53046998+wianpedi@users.noreply.github.com>
2022-02-17 22:36:12 -0500

    Se creo la clase playlist

--- src/musiclibrary/Playlist.java
+++ src/musiclibrary/Playlist.java
@@ -4,10 +4,27 @@
  */
 package musiclibrary;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Wilmer Pérez wianpedi@gmail.com
  */
-public class Playlist {
+public class Playlist extends Library {
+    private String namePlaylist;
+
+    public Playlist(String namePlaylist, ArrayList<Song> songs) {
+        super(songs);
+        this.namePlaylist = namePlaylist;
+    }
+
+    public String getNamePlaylist() {
+        return namePlaylist;
+    }
+    
+    @Override
+    public String toString() {
+        return "Playlist: " + namePlaylist + "\n" + getSongs();
+    }  
     
 }
