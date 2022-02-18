c42af11550beb4b89c9ca9f7846fa58a12dd2248
Andrea Lorena Castro Manzano <91234415+Lcastro98@users.noreply.github.com>
2022-02-18 13:45:42 -0500

    Se documenta la clase Playlist

--- src/musiclibrary/Playlist.java
+++ src/musiclibrary/Playlist.java
@@ -7,21 +7,37 @@ package musiclibrary;
 import java.util.ArrayList;
 
 /**
- *
+ * La clase Playist tiene los atributos y métodos necesarios para crear y obtener la playlist.
+ * 
+ * @version 1.00.00 2022-02-18, la clase corresponde a la versión 1 del sistema.
+ 
  * @author Wilmer Pérez wianpedi@gmail.com
  */
 public class Playlist extends Library {
     private String namePlaylist;
 
+    /**
+     * Constructor: permite crear una instancia de la clase Playlist.
+     * @param namePlaylist
+     * @param songs 
+     */
     public Playlist(String namePlaylist, ArrayList<Song> songs) {
         super(songs);
         this.namePlaylist = namePlaylist;
     }
 
+    /**
+     * Método accesor del atributo namePlaylist.
+     * @return 
+     */
     public String getNamePlaylist() {
         return namePlaylist;
     }
     
+    /**
+     * Sobreescribe el método toString para mostrar los datos de la canción de la manera deseada.
+     * @return 
+     */
     @Override
     public String toString() {
         return "Playlist: " + namePlaylist + "\n" + getSongs();
